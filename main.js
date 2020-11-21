4cae44881a0f43e3e2d5c6bfed8b5283b7ba1f85
kost12-bit <72891196+kost12-bit@users.noreply.github.com>
2020-11-21 17:21:38 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,12 +9,11 @@ let calculator = {
 }
 $("#1").click(function() {
   console.log(1);
-  if (calculator.namberA ===null) {
+  if (calculator.numberA ===null) {
       calculator.numberA = 1;
       calculator.output = 1;
    }
-
-   else {
+  else {
    	calculator.numberB = 1; 
    	calculator.output = 1;
 
@@ -26,29 +25,172 @@ $("#1").click(function() {
 
 $("#2").click(function(){
     console.log(2);
-      if (calculator.namberA ===null) {
+    if (calculator.numberA ===null) {
       calculator.numberA = 2;
       calculator.output = 2;
    }
 
-   else {
+    else {
    	calculator.numberB = 2; 
    	calculator.output = 2;
    }
    output();
 });
+$("#3").click(function(){
+    console.log(3);
+    if (calculator.numberA ===null) {
+      calculator.numberA = 3;
+      calculator.output = 3;
+   }
+
+    else {
+    calculator.numberB = 3; 
+    calculator.output = 3;
+   }
+   output();
+});
+$("#4").click(function(){
+    console.log(4);
+    if (calculator.numberA ===null) {
+      calculator.numberA = 4;
+      calculator.output = 4;
+   }
+
+    else {
+    calculator.numberB = 4; 
+    calculator.output = 4;
+   }
+   output();
+});
+$("#5").click(function(){
+    console.log(5);
+    if (calculator.numberA ===null) {
+      calculator.numberA = 5;
+      calculator.output = 5;
+   }
+
+    else {
+    calculator.numberB = 5; 
+    calculator.output = 5;
+   }
+   output();
+});
+$("#6").click(function(){
+    console.log(6);
+    if (calculator.numberA ===null) {
+      calculator.numberA = 6;
+      calculator.output = 6;
+   }
+
+    else {
+    calculator.numberB = 6; 
+    calculator.output = 6;
+   }
+   output();
+});
+$("#7").click(function(){
+    console.log(7);
+    if (calculator.numberA ===null) {
+      calculator.numberA = 7;
+      calculator.output = 7;
+   }
+
+    else {
+    calculator.numberB = 7; 
+    calculator.output = 7;
+   }
+   output();
+});
+$("#8").click(function(){
+    console.log(8);
+    if (calculator.numberA ===null) {
+      calculator.numberA = 8;
+      calculator.output = 8;
+   }
+
+    else {
+    calculator.numberB = 8; 
+    calculator.output = 8;
+   }
+   output();
+});
+$("#9").click(function(){
+    console.log(9);
+    if (calculator.numberA ===null) {
+      calculator.numberA = 9;
+      calculator.output = 9;
+   }
+
+    else {
+    calculator.numberB = 9; 
+    calculator.output = 9;
+   }
+   output();
+});
+
+
+$("#0").click(function(){
+    console.log(0);
+    if (calculator.numberA ===null) {
+      calculator.numberA = 0;
+      calculator.output = 0;
+   }
+
+    else {
+    calculator.numberB = 0; 
+    calculator.output = 0;
+   }
+   output();
+});
+
+
+
+
 
 $("#plus").click(function(){
     console.log('plus');
     calculator.func = 'plus';
 });
 
+
+$("#del").click(function(){
+    console.log('del');
+    calculator.func = 'del';
+});
+
+
+$("#umn").click(function(){
+    console.log('umn');
+    calculator.func = 'umn';
+});
+
+
+$("#min").click(function(){
+    console.log('min');
+    calculator.func = 'min';
+});
+
+
 $("#enter").click(function(){
     console.log('enter');
     if (calculator.func === 'plus') {
-    	calculator.enter = calculator.numberA + calculator.namberB;
+    	calculator.enter = calculator.numberA + calculator.numberB;
     	calculator.output = calculator.enter;
     }
+
+    if (calculator.func === 'min') {
+      calculator.enter = calculator.numberA - calculator.numberB;
+      calculator.output = calculator.enter;
+    }
+    if (calculator.func === 'del') {
+      calculator.enter = calculator.numberA / calculator.numberB;
+      calculator.output = calculator.enter;
+    }
+    if (calculator.func === 'umn') {
+      calculator.enter = calculator.numberA * calculator.numberB;
+      calculator.output = calculator.enter;
+    }
+    output()
 });
 
 
@@ -56,7 +198,7 @@ $("#enter").click(function(){
 
 
 function output() {
-	$("#output").text(calculator.output)
+	$("#output").text(calculator.output);
 }
 
 
